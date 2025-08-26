683c65d98f8b47c632a6c694bbb4288e8885dec6
GrispenX <145205694+GrispenX@users.noreply.github.com>
2025-08-26 20:04:24 +0300

    Add Lua

--- src/game/client/components/mod_runtime.cpp
+++ src/game/client/components/mod_runtime.cpp
@@ -6,7 +6,7 @@
 
 CModRuntime::CModRuntime()
 {
-
+	m_Lua.set_function("SendChat", [this] (int team, std::string message) {GameClient()->m_Chat.SendChat(team, message.c_str());});
 }
 
 CModRuntime::~CModRuntime()
@@ -24,9 +24,62 @@ void CModRuntime::OnMessage(int Msg, void *pRawMsg)
 	if(Msg == protocol7::NETMSGTYPE_SV_CHAT)
 	{
 		CNetMsg_Sv_Chat *pMsg = (CNetMsg_Sv_Chat *)pRawMsg;
-		if(str_comp(pMsg->m_pMessage, "q") == 0)
+
+		for(auto mod : m_LoadedMods)
 		{
-			GameClient()->m_Chat.SendChat(pMsg->m_Team, pMsg->m_pMessage);		
+			try
+			{
+				m_Lua[mod]["OnMessage"].call(pMsg->m_Team, pMsg->m_pMessage);
+			}
+			catch(...)
+			{
+				log_error("mods", "Something went wrong in mod '%s'", mod);
+			}
 		}
+	}
+}
+
+void CModRuntime::OnInit()
+{	
+	if(!Storage()->FolderExists("mods", IStorage::TYPE_SAVE))
+	{
+		Storage()->CreateFolder("mods", IStorage::TYPE_SAVE);
+	}
+
+	Storage()->ListDirectory(IStorage::TYPE_SAVE, "mods/", OnModFileFoundCallback, this);
+}
+
+int CModRuntime::OnModFileFoundCallback(const char *pFilename, int IsDir, int DirType, void *pUser)
+{
+	CModRuntime* modRuntime = reinterpret_cast<CModRuntime*>(pUser);
+	char modAbsoluteDirPath[1024] = {0};
+	
+	if(!str_endswith(pFilename, ".lua"))
+	{
+		return 0;
+	}
+	
+	modRuntime->Storage()->GetCompletePath(IStorage::TYPE_SAVE, "mods/", modAbsoluteDirPath, sizeof(modAbsoluteDirPath));
+
+	modRuntime->LoadMod(pFilename, modAbsoluteDirPath);
+
+	return 0;
+}
+
+void CModRuntime::LoadMod(const std::string &ModFileName, const std::string &ModDir)
+{
+	const std::string modPath = ModDir + ModFileName;
+	log_info("mods", "Loading mod: %s", modPath.c_str());
+
+	try
+	{
+		sol::object mod = m_Lua.script_file(modPath);
+		m_Lua[ModFileName] = mod;
+	
+		m_LoadedMods.push_back(ModFileName);
+	}
+	catch(...)
+	{
+		log_error("mods", "An error occured while loading mod '%s'", modPath.c_str());
 	}
 }
--- src/game/client/components/mod_runtime.h
+++ src/game/client/components/mod_runtime.h
@@ -2,18 +2,28 @@
 #define GAME_CLIENT_MOD_RUNTIME_H
 
 #include <game/client/component.h>
+#include <sol/sol.hpp>
+#include <vector>
+#include <string>
 
 class CModRuntime : public CComponent
 {
-private:
-    /* data */
 public:
-    CModRuntime(/* args */);
+    CModRuntime();
     ~CModRuntime() override;
-
+    
     int Sizeof() const override;
-
+    
     void OnMessage(int Msg, void *pRawMsg) override;
+    
+    void OnInit() override;
+
+private:
+    static int OnModFileFoundCallback(const char *pFilename, int IsDir, int DirType, void *pUser);
+    void LoadMod(const std::string &ModFileName, const std::string &ModDir);
+
+    sol::state m_Lua;
+    std::vector<std::string> m_LoadedMods;
 };
 
 #endif
